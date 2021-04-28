bdde886b82c9a39bc44c8e205512939be5892e97
Manuel Sanchez del Rio <srio@esrf.eu>
2021-04-28 10:13:39 +0200

    update 1 lens

---
+++ ID18_U18_ONE_LENS/plot_evolution.py
@@ -0,0 +1,118 @@
+import numpy
+from srxraylib.plot.gol import plot
+import matplotlib.pylab as plt
+
+
+subdirectory = "Data7keV_200um"
+beam_dimension_at_slit_in_um = 565
+
+#
+#
+#
+plt.rcParams.update(plt.rcParamsDefault)
+plt.rcParams.update({'figure.autolayout': True})
+
+params = {'legend.fontsize': 15,
+          'legend.frameon': False,
+          'legend.handlelength': 2,
+          # 'axes.titlesize' : 24,
+          'axes.labelsize' :   24,
+          'lines.linewidth' :  3,
+          'lines.markersize' : 10,
+          'xtick.labelsize' :  25,
+          'ytick.labelsize' :  25,
+          # 'grid.color':       'r',
+          # 'grid.linestyle':   '-',
+          # 'grid.linewidth':     2,
+          }
+plt.rcParams.update(params)
+
+
+
+
+
+FACTOR = [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 1.5, 2, 4, 6]
+
+
+
+import scipy.constants as codata
+wavelength = codata.h*codata.c/codata.e/7000
+# a = 234e-6 / 2
+# p = 65.0
+# print("N = ",a**2 / (wavelength * p))
+
+DISTANCE   = []
+FWHM       = []
+ICENTER    = []
+SIGMASF    = []
+LEGEND     = []
+
+for i in range(len(FACTOR)):
+        filein = "%s/aperture_factor_%g.dat" % (subdirectory, FACTOR[i])
+        print(">>>>> ", filein)
+        a1 = numpy.loadtxt(filein)
+        print(a1.shape)
+        distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; icenter1 = a1[:,3]
+        DISTANCE.append(distance1)
+        FWHM.append(fwhm1)
+        ICENTER.append(icenter1)
+        SIGMASF.append(float(FACTOR[i]))
+
+        slit_size_in_um = beam_dimension_at_slit_in_um * float(FACTOR[i])
+        s = slit_size_in_um * 1e-6 / 2
+        p = 65.0
+        pp = 36
+        pa = p - pp
+        # N2 =  p * s ** 2 / (wavelength * pp ** 2)
+        N2 = p * s ** 2 / (wavelength * pa ** 2)
+        print("Effect for slit aperture less than [um] = ", 2e6 * numpy.sqrt(pp ** 2 * wavelength / p))
+
+        LEGEND.append(r'$n$=%5.3g; N=%4.3f' % (FACTOR[i], N2))
+
+        # LEGEND.append(r'$a$=%s a$_{FWHM}$' % (SIGMAS[i]))
+
+fig, ax = plot(   DISTANCE[0], FWHM[0],
+        DISTANCE[1], FWHM[1],
+        DISTANCE[2], FWHM[2],
+        DISTANCE[3], FWHM[3],
+        DISTANCE[4], FWHM[4],
+        DISTANCE[5], FWHM[5],
+        DISTANCE[6], FWHM[6],
+        DISTANCE[7], FWHM[7],
+        ytitle="FWHM [um]",
+        xtitle="Distance from lens [m]",
+        figsize=(15, 4),
+        show=0,
+        legend=LEGEND,
+        ylog=1)
+
+
+# fig, ax = plot(   DISTANCE[0], ICENTER[0],
+#         DISTANCE[1], ICENTER[1],
+#         DISTANCE[2], ICENTER[2],
+#         DISTANCE[3], ICENTER[3],
+#         DISTANCE[4], ICENTER[4],
+#         DISTANCE[5], ICENTER[5],
+#         DISTANCE[6], ICENTER[6],
+#         DISTANCE[7], ICENTER[7],
+#         ytitle="Intensity on axis",
+#         xtitle="Distance from source [m]",
+#         figsize=(15, 4),
+#         show=0,
+#         legend=LEGEND,
+#         ylog=1)
+
+# plt.grid()
+ax.xaxis.grid()
+ax.yaxis.grid()
+
+# F (source): 14.3507, p1: 65, q1: 18.4168
+# F (slit): 14.3507, p1: 29, q1: 28.4089
+ax.plot([18.4168,18.4168], [1e-9,10000])
+ax.plot([28.4089,28.4089], [1e-9,10000])
+
+# file_png = "scan_%s.pdf" % mirror_name
+# plt.savefig(file_png)
+# print("File written to disk: %s" % file_png)
+
+plt.show()
