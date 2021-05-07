a1a03e185e4972818a67f137a5bfe6f898ceee10
Manuel Sanchez Del Rio <srio@esrf.eu>
2021-05-07 17:09:06 +0200

    more

--- ID18_U18_ONE_LENS/plot_waist_size_wofry_vs_hybrid.py
+++ ID18_U18_ONE_LENS/plot_waist_size_wofry_vs_hybrid.py
@@ -6,12 +6,22 @@ FACTOR = [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 1.5] #, 2, 4, 6]
 
 MINFWHM = []
 MINFWHM_HYBRID = []
+MINFWHM_GSM = []
 for i in range(len(FACTOR)):
         filein = "%s/aperture_factor_%g.dat" % ("./7keV_R200um_WofryUnd", FACTOR[i])
         a1 = numpy.loadtxt(filein)
         print(a1.shape)
         distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; icenter1 = a1[:,3]
         MINFWHM.append(fwhm1[numpy.argmin(fwhm1)])
+        print(">>>>> ", i, filein, fwhm1[numpy.argmin(fwhm1)])
+
+        #
+
+        filein = "%s/aperture_factor_%g.dat" % ("./7keV_R200um_WofryGSM", FACTOR[i])
+        a1 = numpy.loadtxt(filein)
+        print(a1.shape)
+        distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; icenter1 = a1[:,3]
+        MINFWHM_GSM.append(fwhm1[numpy.argmin(fwhm1)])
         print(">>>>> ", i, filein, fwhm1[numpy.argmin(fwhm1)])
 
         #
@@ -28,9 +38,10 @@ for i in range(len(FACTOR)):
 beam_dimension_at_slit_in_um = 565
 
 plot(numpy.array(FACTOR),numpy.array(MINFWHM),
+     numpy.array(FACTOR), numpy.array(MINFWHM_GSM),
      numpy.array(FACTOR),numpy.array(MINFWHM_HYBRID),
      numpy.array(FACTOR)[-2:],numpy.array(MINFWHM_HYBRID)[-2:] * 0 + 9.13 * (18.8 / 65),
      numpy.array(FACTOR)[0:2],( numpy.array(FACTOR)[0:2] * beam_dimension_at_slit_in_um) * (28.8 / (65-35) ),
      xtitle="n", ytitle="WAIST FWHM [um]", ylog=1,
      marker=["o","o", "None", "None"],
-     legend=["Wofry","Hybrid","GeomOpt at source","GeomOpt at slit"])
+     legend=["Wofry","WofryGSM","Hybrid","GeomOpt at source","GeomOpt at slit"])
