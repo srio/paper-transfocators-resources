24c4ead58f08b27a494235dc5826f3f27fb17048
Manuel Sanchez del Rio <srio@esrf.eu>
2021-05-09 17:20:42 +0200

    update

--- ID18_U18_ONE_LENS/plot_waist_size_wofry_vs_hybrid.py
+++ ID18_U18_ONE_LENS/plot_waist_size_wofry_vs_hybrid.py
@@ -11,6 +11,7 @@ MINFWHM = []
 MINFWHM_HYBRID = []
 MINFWHM_GSM = []
 MINFWHM_FULLGAUSS = []
+MINFWHM_GAUSSSLIT = []
 for i in range(len(FACTOR)):
         filein = "%s/aperture_factor_%g.dat" % ("./7keV_R200um_WofryUnd", FACTOR[i])
         a1 = numpy.loadtxt(filein)
@@ -26,6 +27,15 @@ for i in range(len(FACTOR)):
         print(a1.shape)
         distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; icenter1 = a1[:,3]
         MINFWHM_GSM.append(fwhm1[numpy.argmin(fwhm1)])
+        print(">>>>> ", i, filein, fwhm1[numpy.argmin(fwhm1)])
+
+        #
+
+        filein = "%s/aperture_factor_%g.dat" % ("./7keV_R200um_GaussianSlit_WofryGSM", FACTOR[i])
+        a1 = numpy.loadtxt(filein)
+        print(a1.shape)
+        distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; icenter1 = a1[:,3]
+        MINFWHM_GAUSSSLIT.append(fwhm1[numpy.argmin(fwhm1)])
         print(">>>>> ", i, filein, fwhm1[numpy.argmin(fwhm1)])
 
         #
@@ -49,10 +59,11 @@ beam_dimension_at_slit_in_um = 565
 
 plot(numpy.array(FACTOR),numpy.array(MINFWHM),
      numpy.array(FACTOR), numpy.array(MINFWHM_GSM),
+     numpy.array(FACTOR), numpy.array(MINFWHM_GAUSSSLIT),
      numpy.array(FACTOR), numpy.array(MINFWHM_FULLGAUSS),
      numpy.array(FACTOR),numpy.array(MINFWHM_HYBRID),
      numpy.array(FACTOR)[-2:],numpy.array(MINFWHM_HYBRID)[-2:] * 0 + 9.13 * (18.8 / 65),
      numpy.array(FACTOR)[0:2],( numpy.array(FACTOR)[0:2] * beam_dimension_at_slit_in_um) * (28.8 / (65-35) ),
-     xtitle="n", ytitle="WAIST FWHM [um]", ylog=1,
-     marker=["o","o", "o", "o", "None","None"],
-     legend=["Wofry","WofryGSM","WofryAllGauss","Hybrid","GeomOpt at source","GeomOpt at slit"])
+     xtitle="n", ytitle="WAIST FWHM [um]", ylog=0,
+     marker=["o","o", "o", "o", "o", "None","None"],
+     legend=["Wofry","WofryGSM", "WofryGaussianSlit", "WofryAllGauss","Hybrid","GeomOpt at source","GeomOpt at slit"])
