cf2535dccc6ae1be2ccecad3b6f7bff89e6ba829
Manuel Sanchez del Rio <srio@esrf.eu>
2021-05-21 20:14:17 +0200

    added scripts

--- ID18_U18_TWO_LENSES/marco_cases_7keV/plot_case7keV_trajectories_f2at170m.py
+++ ID18_U18_TWO_LENSES/marco_cases_7keV/plot_case7keV_trajectories_f2at170m.py
@@ -1,5 +1,5 @@
 import numpy
-from srxraylib.plot.gol import plot
+from srxraylib.plot.gol import plot, set_qt
 from silx.io.specfile import SpecFile
 
 def get_f2(f1=28.2,
@@ -33,18 +33,22 @@ def get_f2(f1=28.2,
 
 if __name__ == "__main__":
 
-    FILEROOT = ["trajectories_h/case7keV_2h","trajectories_v/case7keV_2v"]
-    # fileroot = "trajectories_h/case7keV_2h"
-    # fileroot = "trajectories_v/case7keV_2v"
+    import xraylib
+    set_qt()
+
+    # FILEROOT = ["trajectories_h/case7keV_2h","trajectories_v/case7keV_2v"]
+    FILEROOT = ["sizes_2h/case7keV_2h", "sizes_2v/case7keV_2v"]
+
     nfiles = 200
 
     #
     #
     #
-    Index = numpy.arange(nfiles)
+
 
     for i,fileroot in enumerate(FILEROOT):
 
+        Index = numpy.arange(nfiles)
         if i == 0: # H
             marco_f1 = [41.69,29.73,100.00]
             marco_f2 = [26.15,24.27,25.16]
@@ -52,6 +56,7 @@ if __name__ == "__main__":
             mc = numpy.loadtxt("../../GSM_MC/f1_f2_scans_170_h.dat")
             slit = 40.0
             sourcesize = 70.57
+            prefix = "h"
         else:
             marco_f1 = [49.73,45.71,100]
             marco_f2 = [39.72,57.82,27.16]
@@ -59,42 +64,72 @@ if __name__ == "__main__":
             mc = numpy.loadtxt("../../GSM_MC/f1_f2_scans_170_v.dat")
             slit = 206.8
             sourcesize = 15.02
+            prefix = "v"
 
-        F1 = []
-        F2 = []
-        F2theory1 = []
-        F2theory2 = []
-        R1 = []
-        R2 = []
+
+        #
+        # read files with trajectories
+        #
+        # F1 = []
+        # F2 = []
+        # for index in Index:
+        #     a = numpy.loadtxt("%s_%03d.txt" % (fileroot, index), skiprows=3)
+        #     F2.append(a[1])
+        #     F1.append(a[4])
+
+        a = numpy.loadtxt("trajectories_precalculated/f1_vs_f2_case2%s.dat" % prefix)
+        F1 = a[:, 0].copy()
+        F2 = a[:, 1].copy()
+
+
+
+        #
+        # read or calculate radii
+        #
+        # R1 = []
+        # R2 = []
+        # for index in Index:
+        #     # a = numpy.loadtxt("%s_%03d.txt" % (fileroot, index), skiprows=3)
+        #     # R2.append(a[0])
+        #     # R1.append(a[3])
+        #     xrl_delta = 1.0 - (xraylib.Refractive_Index("Be", 7, 1.85)).real
+        #     R1.append( F1[i] * (2 * xrl_delta))
+        #     R2.append( F2[i] * (2 * xrl_delta))
+
+
+
+        #
+        # read files with sizes
+        #
         FWHM = []
-        Msource_at_id = []
-        Msource_at_slit = []
         for index in Index:
-            a = numpy.loadtxt("%s_%03d.txt" % (fileroot, index), skiprows=3)
-            print(">>>>>> ", a[0], a[1], a[3], a[4])
-            R2.append(a[0])
-            F2.append(a[1])
-            R1.append(a[3])
-            F1.append(a[4])
-
+            print(">>>>>>>>> opening file: ","%s_spectral_density_%03d.dat" % (fileroot, index))
             sf = SpecFile("%s_spectral_density_%03d.dat" % (fileroot, index))
             s1 = sf[0]
             fwhm = s1.scan_header_dict["UFWHM"]
             FWHM.append(float(fwhm))
 
+
+
+        F2theory1 = []
+        F2theory2 = []
+        Msource_at_id = []
+        Msource_at_slit = []
+
+        for index in Index:
             ff_source_at_id, mm_source_at_id = get_f2(f1=F1[index],
                                      position_source=0.0,  # source at source
                                      position_lens1=65.0,
                                      position_lens2=170.0,
                                      position_sample=200.0,
-                                     verbose=True)
+                                     verbose=False)
 
             ff_source_at_slit, mm_source_at_slit = get_f2(f1=F1[index],
                                      position_source=36.0, #0.0,  # source at slit
                                      position_lens1=65.0,
                                      position_lens2=170.0,
                                      position_sample=200.0,
-                                     verbose=True)
+                                     verbose=False)
 
             Msource_at_id.append(mm_source_at_id)
             Msource_at_slit.append(mm_source_at_slit)
@@ -104,7 +139,6 @@ if __name__ == "__main__":
 
 
         print("F:", len(F1), len(F2))
-        print("R:", len(R1), len(R2))
 
         from scipy.signal import savgol_filter
 
@@ -113,10 +147,17 @@ if __name__ == "__main__":
         Msource_at_id = numpy.array(Msource_at_id)
         Msource_at_slit = numpy.array(Msource_at_slit)
 
-        for i in range(15):
-            F2theory1smooth = savgol_filter(F2theory1smooth, 11, 1)  # window size 51, polynomial order 3
-            F2theory2smooth = savgol_filter(F2theory2smooth, 11, 1)  # window size 51, polynomial order 3
-            # Msmooth = savgol_filter(Msmooth, 11, 1)                  # window size 51, polynomial order 3
+        # for i in range(15):
+        #     F2theory1smooth = savgol_filter(F2theory1smooth, 11, 1)  # window size 51, polynomial order 3
+        #     F2theory2smooth = savgol_filter(F2theory2smooth, 11, 1)  # window size 51, polynomial order 3
+        #     # Msmooth = savgol_filter(Msmooth, 11, 1)                  # window size 51, polynomial order 3
+
+
+        print(numpy.array(F1).shape, numpy.array(F2).shape,
+            numpy.array(F1).shape, F2theory1smooth.shape,)
+            # numpy.array(F1).shape, F2theory2smooth.shape,
+            # len(marco_f1), len(marco_f2.shape),
+            # mc[:,0].shape, mc[:,1].shape,)
 
 
         plot(numpy.array(F1), numpy.array(F2),
