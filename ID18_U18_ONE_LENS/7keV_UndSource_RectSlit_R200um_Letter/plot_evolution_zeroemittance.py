07b472b8c5c980e82fd7348b32101e07d7a24850
Manuel Sanchez del Rio <srio@esrf.eu>
2022-04-06 15:38:03 +0200

    working

---
+++ ID18_U18_ONE_LENS/7keV_UndSource_RectSlit_R200um_Letter/plot_evolution_zeroemittance.py
@@ -0,0 +1,217 @@
+import numpy
+from srxraylib.plot.gol import plot, set_qt
+import matplotlib.pylab as plt
+from scipy.signal import savgol_filter
+
+set_qt()
+# subdirectory = "./zeroemittance/" # "Data7keV_200um"
+subdirectory = "/scisoft/users/srio/COMSYL-SLURM/id18_onelens/zeroemittance" # "Data7keV_200um"
+beam_dimension_at_slit_in_um = 565  # needed for calculating Fresnel number
+beam_dimension_at_source = 9.13 # needed for magnification and theoretical sizes
+lens_radius = 200e-6 # for plot title
+
+#
+#
+#
+plt.rcParams.update(plt.rcParamsDefault)
+plt.rcParams.update({'figure.autolayout': True})
+
+params = {'legend.fontsize': 15,
+          'legend.frameon': False,
+          'legend.handlelength': 2,
+          # 'axes.titlesize' : 24,
+          'axes.labelsize' :   24,
+          'lines.linewidth' :  3,
+          'lines.markersize' : 10,
+          'xtick.labelsize' :  25,
+          'ytick.labelsize' :  25,
+          # 'grid.color':       'r',
+          # 'grid.linestyle':   '-',
+          # 'grid.linewidth':     2,
+          }
+plt.rcParams.update(params)
+
+
+
+
+
+FACTOR = [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 1.5] #, 2, 4, 6]
+
+
+import scipy.constants as codata
+wavelength = codata.h*codata.c/codata.e/7000
+size_at_aperture = 565e-6
+# a = 234e-6 / 2
+# p = 65.0
+# print("N = ",a**2 / (wavelength * p))
+
+DISTANCE   = []
+FWHM       = []
+FWHM_SMOOTH       = []
+ICENTER    = []
+SIGMASF    = []
+LEGEND     = []
+FWHM_THEORY_SOURCE       = []
+FWHM_THEORY_SLIT         = []
+FWHM_THEORY_AVERAGE         = []
+FWHM_RATIO_SOURCE         = []
+FWHM_RATIO_SLIT         = []
+APERTURE = []
+BEST_FOCUS_POSITION = numpy.zeros(len(FACTOR))
+BEST_FOCUS_FWHM = numpy.zeros(len(FACTOR))
+BEST_FOCUS_I = numpy.zeros(len(FACTOR), dtype=int)
+N2 = []
+INTENSITY = []
+
+for i in range(len(FACTOR)):
+        filein = "%s/aperture_factor_%g.dat" % (subdirectory, FACTOR[i])
+        print(">>>>> ", filein)
+        a1 = numpy.loadtxt(filein)
+        print(a1.shape)
+        distance1 = a1[:,0]  ;  fwhm1 = a1[:,1] ; intensity1 = a1[:,2] ; icenter1 = a1[:,3]
+        DISTANCE.append(distance1)
+        FWHM.append(fwhm1)
+        FWHM_SMOOTH.append( savgol_filter(savgol_filter(fwhm1, 7, 1), 7, 1) )
+        ICENTER.append(icenter1)
+        SIGMASF.append(float(FACTOR[i]))
+        INTENSITY.append(intensity1[0])
+
+        slit_size_in_um = beam_dimension_at_slit_in_um * float(FACTOR[i])
+        s = slit_size_in_um * 1e-6 / 2
+        p = 65.0
+        pp = 36
+        pa = p - pp
+        # N2 =  p * s ** 2 / (wavelength * pp ** 2)
+        N2.append( p * s ** 2 / (wavelength * pa ** 2) )
+        print("Effect for slit aperture less than [um] = ", 2e6 * numpy.sqrt(pp ** 2 * wavelength / p))
+
+        # magnification, theoretical sizes
+        fwhm_theory_source = beam_dimension_at_source * distance1 / p
+        fwhm_theory_slit = slit_size_in_um * distance1 / pa
+        FWHM_RATIO_SOURCE.append(numpy.abs((fwhm1 - fwhm_theory_source) / fwhm1))
+        FWHM_RATIO_SLIT.append(numpy.abs((fwhm1 - fwhm_theory_slit) / fwhm1))
+        w1 = FACTOR[i]
+        if w1 > 1:
+                w1 = 1
+        w1 = (w1) ** (1/15)
+        w2 = 1.0 - w1
+        wtot = w1 + w2
+        w1 /= wtot
+        w2 /= wtot
+        fwhm_theory_average = fwhm_theory_source * (w1) + fwhm_theory_slit * (w2)
+        # fwhm_theory_average = fwhm_theory_source * (N2/35) + fwhm_theory_slit * (1.0 - (N2/35))
+
+        FWHM_THEORY_SOURCE.append(fwhm_theory_source)
+        FWHM_THEORY_SLIT.append(fwhm_theory_slit)
+        FWHM_THEORY_AVERAGE.append(fwhm_theory_average)
+
+
+        APERTURE.append(size_at_aperture * FACTOR[i])
+
+        i_best_focus = numpy.argmin(FWHM_SMOOTH[i])
+        BEST_FOCUS_I[i] = i_best_focus
+        BEST_FOCUS_FWHM[i] = FWHM_SMOOTH[i][i_best_focus]
+        BEST_FOCUS_POSITION[i] = distance1[i_best_focus]
+
+for i in range(len(FACTOR)):
+        # LEGEND.append(r'$a$=%5.3g $T$=%5.3g; $n$=%5.3g; $N_F$=%4.3f' % (1e6*APERTURE[i],
+        #                                                                 numpy.round(INTENSITY[i] / INTENSITY[-1], 2),
+        #                                                                 FACTOR[i],
+        #                                                                 N2[i]))
+        LEGEND.append(r'$a$=%5.3g $T$=%5.3g; $N_F$=%4.3f' % (1e6*APERTURE[i],
+                                                                        numpy.round(INTENSITY[i] / INTENSITY[-1], 2),
+                                                                        N2[i]))
+
+show_fwhm =  True
+
+if show_fwhm:
+    fig, ax = plot(
+            DISTANCE[0], FWHM_SMOOTH[0], # savgol_filter(savgol_filter(FWHM[0], 7, 1), 7, 1),
+            DISTANCE[1], FWHM_SMOOTH[1], # savgol_filter(savgol_filter(FWHM[1], 7, 1), 7, 1),
+            DISTANCE[2], FWHM_SMOOTH[2], # savgol_filter(savgol_filter(FWHM[2], 7, 1), 7, 1),
+            DISTANCE[3], FWHM_SMOOTH[3], # savgol_filter(savgol_filter(FWHM[3], 7, 1), 7, 1),
+            DISTANCE[4], FWHM_SMOOTH[4], # savgol_filter(savgol_filter(FWHM[4], 7, 1), 7, 1),
+            DISTANCE[5], FWHM_SMOOTH[5], # savgol_filter(savgol_filter(FWHM[5], 7, 1), 7, 1),
+            DISTANCE[6], FWHM_SMOOTH[6], # savgol_filter(savgol_filter(FWHM[6], 7, 1), 7, 1),
+            DISTANCE[7], FWHM_SMOOTH[7], # savgol_filter(savgol_filter(FWHM[7], 7, 1), 7, 1),
+            xrange=[8,52],
+            ytitle=r'FWHM [$\mu$m]',yrange=[1,400],
+            xtitle="Distance from lens [m]",
+            figsize=(15, 4),
+            show=0,
+            legend=LEGEND,
+            ylog=1,)
+
+
+
+else:
+    fig, ax = plot(   DISTANCE[0], ICENTER[0],
+            DISTANCE[1], ICENTER[1],
+            DISTANCE[2], ICENTER[2],
+            DISTANCE[3], ICENTER[3],
+            DISTANCE[4], ICENTER[4],
+            DISTANCE[5], ICENTER[5],
+            DISTANCE[6], ICENTER[6],
+            DISTANCE[7], ICENTER[7],
+            ytitle="Intensity on axis",
+            xtitle="Distance from source [m]",
+            figsize=(15, 4),
+            show=0,
+            legend=LEGEND,
+            ylog=1)
+
+# plt.grid()
+ax.xaxis.grid()
+ax.yaxis.grid()
+# ax.set_title("Be Lens radius: %d um" % (lens_radius * 1e6))
+
+# F (source): 14.3507, p1: 65, q1: 18.4168
+# F (slit): 14.3507, p1: 29, q1: 28.4089
+
+# vertical lines
+energy_keV = 7
+R = lens_radius
+import xraylib
+xrl_delta = 1.0 - (xraylib.Refractive_Index("Be", energy_keV, 1.85)).real
+F = R / (2 * xrl_delta)
+qsrc = 1/(1/F-1/p)
+print("F (source): %g, p1: %g, q1: %g" % (F,p,qsrc))
+p=65 -36.0
+qslt = 1/(1/F-1/p)
+print("F (slit): %g, p1: %g, q1: %g" % (F,p,qslt))
+print("R_Be [mm]= ", 1e3*R)
+
+ax.plot([qsrc,qsrc], [1e-9,10000])
+ax.plot([qslt,qslt], [1e-9,10000])
+ax.plot(BEST_FOCUS_POSITION, BEST_FOCUS_FWHM, color='black', linestyle='dashed')
+
+# file_png = "oneTFund_200um.eps" #"scan_%s.pdf" % mirror_name
+file_png = "oneTF_UndSource_RectSlit_R200um.eps" #"scan_%s.pdf" % mirror_name
+plt.savefig(file_png)
+print("File written to disk: %s" % file_png)
+
+
+plt.show()
+print(">>>", BEST_FOCUS_I[0] )
+print(">>>", DISTANCE[0][BEST_FOCUS_I[0]])
+print(">>>", FWHM_THEORY_SLIT[0][BEST_FOCUS_I[0]] )
+xx = DISTANCE[0][BEST_FOCUS_I[0]]
+yy = FWHM_THEORY_SLIT[0][BEST_FOCUS_I[0]]
+plot(BEST_FOCUS_POSITION, BEST_FOCUS_FWHM,
+        DISTANCE[0][BEST_FOCUS_I[0]], FWHM_THEORY_SLIT[0][BEST_FOCUS_I[0]],
+        DISTANCE[1][BEST_FOCUS_I[1]], FWHM_THEORY_SLIT[1][BEST_FOCUS_I[1]],
+        DISTANCE[2][BEST_FOCUS_I[2]], FWHM_THEORY_SLIT[2][BEST_FOCUS_I[2]],
+        DISTANCE[3][BEST_FOCUS_I[3]], FWHM_THEORY_SLIT[3][BEST_FOCUS_I[3]],
+        DISTANCE[4][BEST_FOCUS_I[4]], FWHM_THEORY_SLIT[4][BEST_FOCUS_I[4]],
+        DISTANCE[5][BEST_FOCUS_I[5]], FWHM_THEORY_SLIT[5][BEST_FOCUS_I[5]],
+        DISTANCE[6][BEST_FOCUS_I[6]], FWHM_THEORY_SLIT[6][BEST_FOCUS_I[6]],
+        DISTANCE[0][BEST_FOCUS_I[0]], FWHM_THEORY_SOURCE[0][BEST_FOCUS_I[0]],
+        DISTANCE[1][BEST_FOCUS_I[1]], FWHM_THEORY_SOURCE[1][BEST_FOCUS_I[1]],
+        DISTANCE[2][BEST_FOCUS_I[2]], FWHM_THEORY_SOURCE[2][BEST_FOCUS_I[2]],
+        DISTANCE[3][BEST_FOCUS_I[3]], FWHM_THEORY_SOURCE[3][BEST_FOCUS_I[3]],
+        DISTANCE[4][BEST_FOCUS_I[4]], FWHM_THEORY_SOURCE[4][BEST_FOCUS_I[4]],
+        DISTANCE[5][BEST_FOCUS_I[5]], FWHM_THEORY_SOURCE[5][BEST_FOCUS_I[5]],
+        DISTANCE[6][BEST_FOCUS_I[6]], FWHM_THEORY_SOURCE[6][BEST_FOCUS_I[6]],
+        marker=[None, 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+        xrange=[15,30], )
+
---
+++ ID18_U18_TWO_LENSES/letter_cases_7keV/plot_combined_results.py
@@ -0,0 +1,177 @@
+import numpy
+from srxraylib.plot.gol import plot, plot_image
+from silx.io.specfile import SpecFile
+from oasys.util.oasys_util import get_fwhm
+from srxraylib.util.h5_simple_writer import H5SimpleWriter
+import h5py
+from plot_analytical import get_f2
+
+def get_data_from_h5(filename, aperture):
+    f = h5py.File(filename, 'r')
+    entry = "f1f2map_a=%4.2f" % (1e6 * aperture)
+    F1F2_FWHM = f["%s/FWHM/image_data" % entry][()].T
+    F1F2_I0 = f["%s/I0/image_data" % entry][()].T
+    F1F2_PEAK = f["%s/PEAK/image_data" % entry][()].T
+    F1 = f["%s/FWHM/axis_x" % entry][()]
+    F2 = f["%s/FWHM/axis_y" % entry][()]
+    f.close()
+
+    ibest = numpy.argmax(F1F2_I0, axis=1)
+    fwhm_best = numpy.zeros_like(F1)
+    for i in range(F1.size):
+        fwhm_best[i] = F1F2_FWHM[i, ibest[i]]
+
+
+    return {"F1":F1,
+            "F2":F2,
+            "F1F2_FWHM":F1F2_FWHM,
+            "F1F2_I0":F1F2_I0,
+            "F1F2_PEAK":F1F2_PEAK,
+            "ibest":ibest,
+            "fwhm_best":fwhm_best}
+
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+
+
+
+
+    direction = 'H'
+
+    #
+    #  theoretical
+    #
+
+    F1 = numpy.linspace(5, 100, 500)
+    F2_Source = numpy.zeros_like(F1)
+    F2_Slit = numpy.zeros_like(F1)
+
+    for index, f1 in enumerate(F1):
+        ff_id, mm_source_at_id = get_f2(f1=f1,
+                                        position_source=0.0,  # source at source
+                                        position_lens1=65.0,
+                                        position_lens2=170.0,
+                                        position_sample=200.0,
+                                        verbose=False)
+        ff_slit, mm_source_at_slit = get_f2(f1=F1[index],
+                                            position_source=36.0,  # 0.0,  # source at slit
+                                            position_lens1=65.0,
+                                            position_lens2=170.0,
+                                            position_sample=200.0,
+                                            verbose=False)
+
+        if ff_id < 0:
+            ff_id = numpy.nan
+            mm_source_at_id = numpy.nan
+
+        if ff_slit < 0:
+            ff_slit = numpy.nan
+            mm_source_at_slit = numpy.nan
+
+        F2_Source[index] = ff_id
+        F2_Slit[index] = ff_slit
+
+    #
+    # matplotlib settings
+    #
+
+    plt.rcParams.update(plt.rcParamsDefault)
+    plt.rcParams.update({'figure.autolayout': True})
+
+    params = {'legend.fontsize': 15,
+              'legend.frameon': False,
+              'legend.handlelength': 2,
+              'axes.titlesize' : 24,
+              'axes.labelsize': 24,
+              'lines.linewidth': 3,
+              'lines.markersize': 10,
+              'xtick.labelsize': 25,
+              'ytick.labelsize': 25,
+              # 'grid.color':       'r',
+              # 'grid.linestyle':   '-',
+              # 'grid.linewidth':     2,
+              }
+    plt.rcParams.update(params)
+
+
+    if direction == 'V':
+
+        APERTURE = [25e-6, 227.0e-6, 506.7e-6, 1500e-6]   # vertical
+
+        for i,aperture in enumerate(APERTURE):
+
+            d0 = get_data_from_h5("/scisoft/users/srio/data_id18/trajectories_precalculated_mapping/f1f2map_v.h5", aperture)
+            d1 = get_data_from_h5("/scisoft/users/srio/data_id18/trajectories_precalculated_mapping/f1f2map_firstbranch_v.h5", aperture)
+            d2 = get_data_from_h5("/scisoft/users/srio/data_id18/trajectories_precalculated_mapping/f1f2map_secondbranch_v.h5", aperture)
+
+
+            fig, ax = plot_image(d0["F1F2_I0"], d0["F1"], d0["F2"],
+                       title="$a_v$=%g $\mu$m" % (1e6*aperture),
+                       xtitle="$f_1$ [m]", ytitle="$f_2$ [m]",
+                       add_colorbar=0, figsize=(8,8),
+                       aspect='auto', show=0)
+
+
+            ax.plot(F1, F2_Source, color='white', linestyle=(0, (1, 10)))
+            ax.plot(F1, F2_Slit, color='white', linestyle=(0, (5, 10)))
+            ax.plot(d0["F1"][0::4], d0["F2"][d0["ibest"]][0::4], color='red', linestyle="", marker='.')
+
+
+            filename = "V_%d.png" % i
+            plt.savefig(filename)
+            print("File written to disk: %s" % filename)
+
+            # plot(d0["F1"], d0["F2"][d0["ibest"]],
+            #      # d1["F1"], d1["F2"][d1["ibest"]],
+            #      # d2["F1"], d2["F2"][d2["ibest"]],
+            #      title="trajectories", xtitle="f1", ytitle="f2", show=0)
+            #
+            # plot(d0["F1"], d0["fwhm_best"],
+            #      # d1["F1"], d1["fwhm_best"],
+            #      # d2["F1"], d2["fwhm_best"],
+            #      title="beam size", xtitle="f1", ytitle="fwhm", ylog=1, show=0) #, yrange=[1e0,1e3])
+
+            plt.show()
+
+    elif direction == 'H':
+        APERTURE = [40.3e-6, 85.1e-6, 145.5e-6, 1000e-6]  # horizontal
+
+        for i,aperture in enumerate(APERTURE):
+            d0 = get_data_from_h5("/scisoft/users/srio/data_id18/trajectories_precalculated_mapping/f1f2map_h.h5",
+                                  aperture)
+            d1 = get_data_from_h5("/scisoft/users/srio/data_id18/trajectories_precalculated_mapping/f1f2map_h_old.h5",
+                                  aperture)
+
+
+            fig , ax = plot_image(d0["F1F2_I0"], d0["F1"], d0["F2"],
+                       title="$a_h$=%g $\mu$m" % (1e6*aperture),
+                       xtitle="$f_1$ [m]", ytitle="$f_2$ [m]",
+                       add_colorbar=0, figsize=(8, 8),
+                       aspect='auto', show=0)
+
+
+
+            ax.plot(F1, F2_Source, color='white', linestyle=(0, (1, 10)))
+            ax.plot(F1, F2_Slit, color='white', linestyle=(0, (5, 10)))
+            ax.plot(d0["F1"][0::4], d0["F2"][d0["ibest"]][0::4], color='red', linestyle="", marker='.')
+
+            filename = "H_%d.png" % i
+            plt.savefig(filename)
+            print("File written to disk: %s" % filename)
+
+            # plot_image(d1["F1F2_I0"], d1["F1"], d1["F2"],
+            #            title="OLD!!!!!!!! %s  I0 aperture=%g" % (direction, aperture),
+            #            aspect='auto', show=0)
+            #
+            # plot(d0["F1"], d0["F2"][d0["ibest"]],
+            #      d1["F1"], d1["F2"][d1["ibest"]],
+            #      # d2["F1"], d2["F2"][d2["ibest"]],
+            #      xtitle="f1", ytitle="f2", show=0)
+            #
+            # plot(d0["F1"], d0["fwhm_best"],
+            #      d1["F1"], d1["fwhm_best"],
+            #      # d2["F1"], d2["fwhm_best"],
+            #      xtitle="f1", ytitle="fwhm", ylog=0)  # , yrange=[1e0,1e3])
+
+            plt.show()
